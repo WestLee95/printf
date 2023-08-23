dada49ccb52616c034128b24fb8a665f100d829b
Jane <waguthijane61@gmail.com>
2023-08-22 23:54:53 -0700

    first

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 This handles our printf project
 Hey printf partener two is here
+/**********starting point*************/
+0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life
+	Function that produces output according to a format
+
+1. Education is when you read the fine print. Experience is what you get if you don't
+	Handling the conversion specifiers d and i
+
+3. What one has not experienced, one will never understand in print
+	Handling the conversion specifiers u, o, x and X
+
+4. Nothing in fine print is ever good news
+	Use a local buffer of 1024 chars in order to call write as little as possible
+
+5. My weakness is wearing too much leopard print
+	Handling the custom conversion specifier S which prints the string.
+
+6. How is the world ruled and led to war? Diplomats lie to journalists and believe these lies when they see them in print
+	Handling the conversion specifier p
+
+7. The big print gives and the small print takes away
+	Handling the flag characters +, space and # for non-custom conversion specifiers
+
+8. Sarcasm is lost in print
+	Handling the length modifiersl and h for non-custom conversion specifiers
+
+9. Print some money and give it to us for the rain forests
+	Handle the field width for non-custom conversion specifiers.
+
+10. The negative is the equivalent of the composer's score, and the print the performance
+	Handling the precision for non-custom conversion specifiers.
+
+11. It's depressing when you're still around and your albums are out of print
+	Handling the 0 flag character for non-custom conversion specifiers.
+
+12. Every time that I wanted to give up, if I saw an interesting textile, print what ever, suddenly I would see a collection
+	Handle the - flag character for non-custom conversion specifiers.
+
+13. Print is the sharpest and the strongest weapon of our party
+	Handling the custom conversion specifier r which prints the reversed string
+
+14. The flood of print has turned reading into a process of gulping rather than savoring
+	Handling the custom conversion specifier R which  prints the rot13'ed string
+
+15. *
+	All the above options work well together.
--- function2.c
+++ function2.c
@@ -1,35 +1,186 @@
 #include "main.h"
 
+/********* PRINT POINTERS **********/
 /**
- * print_bi - Convert decimal to binary
- * @n:Decimal number
- * Return: Number of characters printed or 0
+ * print_pointer - Prints the value of a pointer variable
+ * @types: List of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of character  printed or 0
  */
-int print_bi(unsigned int n)
+int print_pointer(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
-	int i, index = 0, count = 0, binary[32];
+	char extra_c = 0, padd = ' ';
+	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
+	unsigned long num_addrs;
+	char map_to[] = "0123456789abcdef";
+	void *addrs = va_arg(types, void *);
 
-	if (n == 0)
+	UNUSED(width);
+	UNUSED(size);
+
+	if (addrs == NULL)
+		return (write(1, "(nil)", 5));
+
+	buffer[BUFF_SIZE - 1] = '\0';
+	UNUSED(precision);
+
+	num_addrs = (unsigned long)addrs;
+
+	while (num_addrs > 0)
 	{
-		putchar('0');
+		buffer[ind--] = map_to[num_addrs % 16];
+		num_addrs /= 16;
+		length++;
+	}
+
+	if ((flags & F_ZERO) && !(flags & F_MINUS))
+		padd = '0';
+	if (flags & F_PLUS)
+		extra_c = '+', length++;
+	else if (flags & F_SPACE)
+		extra_c = ' ', length++;
+
+	ind++;
+
+	return (write_pointer(buffer, ind, length,
+		width, flags, padd, extra_c, padd_start));
+}
+
+/************************* PRINT NON PRINTABLE *************************/
+/**
+ * print_non_printable - Prints ascii codes in hexa of non printable chars
+ * @types: List of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width:width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of character  printed or 0
+ */
+int print_non_printable(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int i = 0, offset = 0;
+	char *str = va_arg(types, char *);
+
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	if (str == NULL)
+		return (write(1, "(null)", 6));
+
+	while (str[i] != '\0')
+	{
+		if (is_printable(str[i]))
+			buffer[i + offset] = str[i];
+		else
+			offset += append_hexa_code(str[i], buffer, i + offset);
+
+		i++;
+	}
+
+	buffer[i + offset] = '\0';
+
+	return (write(1, buffer, i + offset));
+}
+
+/************** PRINTS STRING IN  REVERSE **************/
+/**
+ * print_reverse - Prints reverse string.
+ * @types: List of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Numbers of character printed or 0
+ */
+
+int print_reverse(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	char *str;
+	int i, count = 0;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(size);
+
+	str = va_arg(types, char *);
+
+	if (str == NULL)
+	{
+		UNUSED(precision);
+
+		str = ")Null(";
+	}
+	for (i = 0; str[i]; i++)
+		;
+
+	for (i = i - 1; i >= 0; i--)
+	{
+		char z = str[i];
+
+		write(1, &z, 1);
 		count++;
 	}
-	else
+	return (count);
+}
+/************************* PRINT A STRING IN ROT13 *************************/
+/**
+ * print_rot13string - Print a string in rot13.
+ * @types: List of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width:width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Numbers of character  printed or 0
+ */
+int print_rot13string(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	char x;
+	char *str;
+	unsigned int i, j;
+	int count = 0;
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	str = va_arg(types, char *);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	if (str == NULL)
+		str = "(AHYY)";
+	for (i = 0; str[i]; i++)
 	{
-
-		while (n > 0)
+		for (j = 0; in[j]; j++)
 		{
-			binary[index] = n % 2;
-			n /= 2;
-			index++;
+			if (in[j] == str[i])
+			{
+				x = out[j];
+				write(1, &x, 1);
+				count++;
+				break;
+			}
 		}
-
-		for (i = index - 1; i >= 0; i--)
+		if (!in[j])
 		{
-			putchar(binary[i] + '0');
+			x = str[i];
+			write(1, &x, 1);
 			count++;
 		}
 	}
-
 	return (count);
-}
