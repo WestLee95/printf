486425d90bdf84df926ee9afc5cf9d9b538cf5b2
Jane <waguthijane61@gmail.com>
2023-08-22 22:19:14 -0700

    first

--- main.h
+++ main.h
@@ -9,8 +9,31 @@
 #include <unistd.h>
 
 int _putchar(char c);
+int print_number(int n);
 int _printf(const char *format, ...);
-void print_binary(unsigned int num);
+int print_octal(unsigned int n);
+int print_unsig(unsigned int n);
+int print_hexaup(unsigned int n);
+int print_hexalow(unsigned int n);
+int print_bi(unsigned int n);
+int _printstring(char *s);
+int rot13(char *point);
+int print_rev(char *s);
+int print_hl(uintptr_t n);
+int print_bigS(char *);
+int main(void);
 
-
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
 #endif
